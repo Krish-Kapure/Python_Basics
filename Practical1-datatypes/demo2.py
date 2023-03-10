48d7afadfad6b123d056718e27db40a7978f7262
Krishna Kapure <Krishna.Kapure@einfochips.com>
2023-03-10 15:33:19 +0530

    F_COMMIT

---
+++ Practical1-datatypes/demo2.py
@@ -0,0 +1,15 @@
+colors = ["red", "blue", "Green"]
+print(colors)
+
+colors.insert(1,"black")
+print(colors)
+
+
+students = {
+    "name": "Krishna",
+    "emailId": "krishkapure1996@gmail.com",
+    "marks": [91, 92, 93, 94, 95]
+}
+print(students["marks"][1])
+
+
---
+++ Practical3-logics/Task_2.py
@@ -0,0 +1,22 @@
+custName = "Chii"
+itemDesc = ["shirt", "jeans", "glasses", "watch"]
+itemNumber = 5
+price = 500
+tax = 25.78
+quantity = 2
+outOfStock = False
+
+if itemNumber < len(itemDesc):
+    if outOfStock:
+        print("selected item is unavailable")
+    else:
+        message = custName + " wants to purchase " + str(quantity) + " " + itemDesc[0]
+        if quantity > 1:
+            print(message + "s")
+        else:
+            print(message)
+        print("total cost with tax is:", quantity * (price + tax))
+else:
+    message = "Not selected correct item code"
+    print(message)
+
